698f552d90b0b8dcaed02891a02df3976d83e00e
jonathananolan <jonathan.a.nolan@gmail.com>
2024-02-26 03:04:32 -0500

    added s3 details

--- run_rmd.R
+++ run_rmd.R
@@ -1,10 +1,19 @@
-walk(list.files(path = "R/",
-                pattern = "*.R",
-                full.names = T),source)
+library(purrr)
 
+# Start by importing all the functions that make this project tick. 
+# The most important is 00renv.R which you'll want to look at - it points to the source file as well as 
+# loading it into memory. 
+# The rest are functions which run only when called later on in the Rmarkdown. 
 
+r_files <- list.files(path = "R/",
+                      pattern = "*.R$",
+                      full.names = T)
+
+walk(r_files,source)
+
+#Add beskope info on how YIMBY Melbourne wants to re-zone Melbourne
 sf_mel_props <- add_missing_middle_zoning_info() 
-
+#Create a version without geometries
 df_mel_props <- sf_mel_props %>% st_drop_geometry() 
 
 sf::sf_use_s2(FALSE)  # Disable s2 engine
@@ -16,7 +25,7 @@ lgas <- df_mel_props %>%
   filter(n > 10000) %>% 
   pull(lga_name_2022)
 
-
+#We want to look at all LGAs but greenfield. We really don't have a solution for Greenfield right now. 
 missing_middle_lgas <- df_mel_props %>% 
   st_drop_geometry() %>% 
   group_by(lga_name_2022,area) %>% 
@@ -26,6 +35,7 @@ missing_middle_lgas <- df_mel_props %>%
   filter(area != "greenfield") %>%
   pull(lga_name_2022)
 
+#filter the big dataset for a given LGA (area name) and then render the rmarkdown, saving it into the RMD folder.
 
 run_for_area <- function(area_name) {
   
@@ -54,12 +64,16 @@ run_for_area <- function(area_name) {
 
 walk(missing_middle_lgas,run_for_area)
 
-
+#Create the index page as well
 
 rmarkdown::render("index.Rmd", 
                   params = list(area = area_name),
                   output_file = paste0("rmd/index.html")
 )
+
+
+#Upload to AWS - let JN know if you want access to the bucket. If not commit your changes and he can run. 
+ if(Sys.info()[7] == "jonathannolan") {
 
 list_rmds_with_path <- list.files(pattern = "*.html",
                         path = "rmd",
@@ -68,10 +82,19 @@ list_rmds_with_path <- list.files(pattern = "*.html",
 list_rmds <- list.files(pattern = "*.html",
                         path = "rmd")
 
+
+aws.s3::bucketlist(add_region = T)
+Sys.setenv("AWS_DEFAULT_REGION" = "ap-southeast-2")
+
+Sys.setenv(AWS_DEFAULT_REGION = rstudioapi::askForPassword())
+Sys.setenv(AWS_ACCESS_KEY_ID = rstudioapi::askForPassword())
+
 upload_object <- function(file_location,url){
 
-put_object(file = file_location, object = url, bucket = "yimby-mel",multipart = T,show_progress = T,
-           headers = list("Content-Type" = "text/html"))
+put_object(file = file_location, bucket = "yimby-mel",multipart = T,show_progress = T,
+           headers = list("Content-Type" = "text/html"),verbose = T)
 }
 
-walk2(list_rmds_with_path,list_rmds,upload)
+walk2(list_rmds_with_path,list_rmds,upload_object)
+
+}
