c85efb019613442fbba40e96a864567542f7a59c
jonathananolan <jonathan.a.nolan@gmail.com>
2024-04-10 21:40:56 -0400

    added theme issues for docker

--- theme/yimby_melbounre_ggplot_theme.R
+++ theme/yimby_melbounre_ggplot_theme.R
@@ -49,7 +49,7 @@ theme_yimby_mel_caption <- function(caption_text = " ",
   
   if(text_size == "big") {font_size = 70;  pic_height = 50  } else {font_size = 12;  pic_height = 20}
   
-  if(computer_type == 'Linux'){font_size = 70}
+  if(text_size == 'Linux' & text_size == "giant"){font_size = 70}
   
   update_geom_defaults("bar",  list(fill = yimby_colours$green_palette[1], colour = yimby_colours$green_palette[1]))
   update_geom_defaults("col",  list(fill = yimby_colours$green_palette[1], colour = yimby_colours$green_palette[1]))
